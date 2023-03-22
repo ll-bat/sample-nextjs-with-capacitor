a87ec98c6910b5d18c7985934c5d4c4ddd6fe74f
vladimer <gshatakishvili@gmail.com>
2023-03-22 13:39:43 +0400

    Add android and ios platforms and update capacitor.config.js

---
+++ android/app/src/main/java/com/nextjs_with_capacitor/sample/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nextjs_with_capacitor.sample;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nextjs_with_capacitor.sample',
+  appName: 'sample-nextjs-app-with-capacitor-for-deployment',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
