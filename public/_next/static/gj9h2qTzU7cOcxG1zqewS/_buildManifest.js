18f48d7c42c6ed76e17cf30a31f34fcd01920bed
vladimer <gshatakishvili@gmail.com>
2023-03-22 14:44:30 +0400

    Install ionic and make some modifications

---
+++ public/_next/static/gj9h2qTzU7cOcxG1zqewS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/299b4710ca2a9192.css","static/chunks/pages/index-f265741ffd83eed4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
